be26808dfa962495bf8f7d5be384b2a767a9aa73
Jerico-code <131560114+Jerico-code@users.noreply.github.com>
2025-03-19 15:55:55 +0000

    Update sarno.sql
    Aggiunta chiavi primarie, chiavi esterne e auto incrementi

--- sarno.sql
+++ sarno.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 19, 2025 at 12:47 PM
+-- Generation Time: Mar 19, 2025 at 04:53 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,15 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `amministratore`
 --
 
-CREATE TABLE `admin` (
+CREATE TABLE `amministratore` (
   `idAdmin` int(38) NOT NULL,
   `Nome` varchar(100) NOT NULL,
   `Cognome` varchar(100) NOT NULL,
   `Email` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL
+  `Password` varchar(255) NOT NULL,
+  `idRuolo` int(38) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `materia`
+--
+
+CREATE TABLE `materia` (
+  `idMateria` int(38) NOT NULL,
+  `materia` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
@@ -46,25 +58,133 @@ CREATE TABLE `professore` (
   `nome` varchar(100) NOT NULL,
   `cognome` varchar(100) NOT NULL,
   `classi` varchar(20) NOT NULL,
-  `materie` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `idRuolo` int(38) NOT NULL,
+  `idMateria` int(38) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `studente`
+-- Table structure for table `ruoli`
 --
 
-CREATE TABLE `studente` (
+CREATE TABLE `ruoli` (
+  `idRuolo` int(38) NOT NULL,
+  `ruolo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studenti`
+--
+
+CREATE TABLE `studenti` (
   `idStudente` int(38) NOT NULL,
   `nome` varchar(100) DEFAULT NULL,
   `cognome` varchar(100) DEFAULT NULL,
   `classe` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `idRuolo` int(38) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `amministratore`
+--
+ALTER TABLE `amministratore`
+  ADD PRIMARY KEY (`idAdmin`),
+  ADD KEY `idRuoloAdmin` (`idRuolo`);
+
+--
+-- Indexes for table `materia`
+--
+ALTER TABLE `materia`
+  ADD PRIMARY KEY (`idMateria`);
+
+--
+-- Indexes for table `professore`
+--
+ALTER TABLE `professore`
+  ADD PRIMARY KEY (`idProfessore`),
+  ADD KEY `idRuoloProfessore` (`idRuolo`),
+  ADD KEY `idMateriaProfessore` (`idMateria`);
+
+--
+-- Indexes for table `ruoli`
+--
+ALTER TABLE `ruoli`
+  ADD PRIMARY KEY (`idRuolo`);
+
+--
+-- Indexes for table `studenti`
+--
+ALTER TABLE `studenti`
+  ADD PRIMARY KEY (`idStudente`),
+  ADD KEY `idRuoloStudente` (`idRuolo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `amministratore`
+--
+ALTER TABLE `amministratore`
+  MODIFY `idAdmin` int(38) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `materia`
+--
+ALTER TABLE `materia`
+  MODIFY `idMateria` int(38) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `professore`
+--
+ALTER TABLE `professore`
+  MODIFY `idProfessore` int(38) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ruoli`
+--
+ALTER TABLE `ruoli`
+  MODIFY `idRuolo` int(38) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `studenti`
+--
+ALTER TABLE `studenti`
+  MODIFY `idStudente` int(38) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `amministratore`
+--
+ALTER TABLE `amministratore`
+  ADD CONSTRAINT `idRuoloAdmin` FOREIGN KEY (`idRuolo`) REFERENCES `ruoli` (`idRuolo`);
+
+--
+-- Constraints for table `professore`
+--
+ALTER TABLE `professore`
+  ADD CONSTRAINT `idMateriaProfessore` FOREIGN KEY (`idMateria`) REFERENCES `materia` (`idMateria`),
+  ADD CONSTRAINT `idRuoloProfessore` FOREIGN KEY (`idRuolo`) REFERENCES `ruoli` (`idRuolo`);
+
+--
+-- Constraints for table `studenti`
+--
+ALTER TABLE `studenti`
+  ADD CONSTRAINT `idRuoloStudente` FOREIGN KEY (`idRuolo`) REFERENCES `ruoli` (`idRuolo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
